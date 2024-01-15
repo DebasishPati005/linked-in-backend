de08927c9bfd7d89f626114cb1606d0e7ad9428a
DebasishPatiITT <debasish.pati@intimetec.com>
2024-01-15 11:04:45 +0530

    db url and type changed

--- linked-in-clone-server/src/main.ts
+++ linked-in-clone-server/src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api-docs', app, document);
 
   // Logging API record
   app.use(morgan('tiny', { stream: apiLogStream }));
